ab8110266c41af3773971d810e750a90006dfe0c
Osvaldo <pagioliosvaldo@gmail.com>
2025-02-27 15:12:15 -0300

    Adicionado dados do vetor com 1M de elementos + gráficos individuais

--- Graficos.R
+++ Graficos.R
@@ -2,9 +2,10 @@ library(ggplot2)
 
 tExec <- read.csv("resultados.csv")
 
-ggplot(data = tExec, aes(x = tamanho))+
-  geom_line(aes(y=tempoNaive), color = "red")+
-  geom_line(aes(y=tempoDac), color = "blue") +
+ggplot(data = tExec, aes(x = tamanho, y = tempoDac))+
+  ##geom_line(aes(y=tempoNaive), color = "red")+
+  ##geom_line(aes(y=tempoDac), color = "blue") +
+  geom_line(color = "blue")+
   scale_y_time(name = "Tempo de execução")+
   scale_x_continuous(name = "Quantidade de elementos") +
   theme_bw()
