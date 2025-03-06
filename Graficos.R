db175672005362f5131a38aaf3fe74b7e0a64d6b
Osvaldo <pagioliosvaldo@gmail.com>
2025-03-06 18:28:01 -0300

    Plots atualizados.

--- Graficos.R
+++ Graficos.R
@@ -4,8 +4,9 @@ tExec <- read.csv("resultados.csv")
 
 ggplot(data = tExec, aes(x = tamanho))+
   geom_line(aes(y=tempoNaive), color = "red")+
-  geom_line(aes(y=tempoDac), color = "blue") +
-  geom_line(color = "blue")+
+  geom_line(aes(y=tempoDac), color = "blue", alpha=0.4,linewidth=2) +
+  geom_line(aes(y=tempoDacMelhorado),color="green", alpha=0.5)+
+  #geom_line(color = "blue")+
   scale_y_time(name = "Tempo de execução")+
   scale_x_continuous(name = "Quantidade de elementos") +
   theme_bw()
