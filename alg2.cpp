6084bbec616c283181c2d6833ab514efaa49dcd3
Marcos Paulo Gomes <128519024+Marcospgp26@users.noreply.github.com>
2025-03-06 14:43:28 -0300

    Update alg2.cpp

--- alg2.cpp
+++ alg2.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-vector<int> Gerador_De_Vetor(int tamanho){
+vector<int> Gerador_De_Vetor_Maximal(int tamanho)
+{
     srand(time(NULL));
 
     vector<int> Vetor(tamanho);
@@ -12,10 +13,12 @@ vector<int> Gerador_De_Vetor(int tamanho){
     int NumeroPosicoes = floor(tamanho/2) + 1;
     int NumeroMaisRep = rand()%100;
 
-    for(int i = 0; i < NumeroPosicoes; i++){
+    for(int i = 0; i < NumeroPosicoes; i++)
+    {
         int posicao = rand()%tamanho;
 
-        while(VetorPosicao[posicao]){
+        while(VetorPosicao[posicao])
+        {
             posicao++;
             posicao %= tamanho;
         }
@@ -26,7 +29,8 @@ vector<int> Gerador_De_Vetor(int tamanho){
 
     for(int i = 0; i < tamanho; i++)
     {
-        if(!VetorPosicao[i]){
+        if(!VetorPosicao[i])
+        {
             int elemento = rand()%100;
             Vetor[i] = elemento;
         }
@@ -36,8 +40,20 @@ vector<int> Gerador_De_Vetor(int tamanho){
     return Vetor;
 }
 
+vector<int> Gerador_De_Vetor(int tamanho)
+{
+    vector<int> v;
+    srand(time(NULL));
+    for(int i = 0; i < tamanho; i++)
+    {
+        v.push_back(rand()%100);
+    }
+    return v;
+}
 
-int NumeroMaisRepetidoIterativo(vector<int>& vetor, int tamanho){
+//-------------------------------------------------------------------------------------------------------------------------------
+int majorIterativo(vector<int>& vetor, int tamanho)
+{
     int NumeroMaisRepetido = 0;
     int ContadorTotal = 0;
 
@@ -45,76 +61,172 @@ int NumeroMaisRepetidoIterativo(vector<int>& vetor, int tamanho){
     {
         int ContadorAtual = 1;
 
-            for(int j = i + 1; j < tamanho; j++)
-            {
-                if(vetor[j] == vetor[i]) ContadorAtual++;
-            }
-            if(ContadorAtual > ContadorTotal)
-            {
-                ContadorTotal = ContadorAtual;
-                NumeroMaisRepetido = vetor[i];
-            }
+        for(int j = i + 1; j < tamanho; j++)
+        {
+            if(vetor[j] == vetor[i]) ContadorAtual++;
+        }
+        if(ContadorAtual > ContadorTotal)
+        {
+            ContadorTotal = ContadorAtual;
+            NumeroMaisRepetido = vetor[i];
+        }
 
     }
     return NumeroMaisRepetido;
 }
 
-int NumeroMaisRepetidoDivAndConquer(vector<int>& vetor, int inicio, int fim) {
+//-------------------------------------------------------------------------------------------------------------------------------
+int elementoMajoritarioDaC(vector<int>& vetor, int inicio, int fim)
+{
     // Caso base: Se há apenas um elemento
-    if (inicio == fim) {
+    if (inicio == fim)
+    {
         return vetor[inicio];
     }
 
     // Dividir o vetor em duas metades
     int meio = inicio + (fim - inicio) / 2;
-    int numeroEsquerda = NumeroMaisRepetidoDivAndConquer(vetor, inicio, meio);
-    int numeroDireita = NumeroMaisRepetidoDivAndConquer(vetor, meio + 1, fim);
+    int numeroEsquerda = elementoMajoritarioDaC(vetor, inicio, meio);
+    int numeroDireita = elementoMajoritarioDaC(vetor, meio + 1, fim);
 
     // Contar as ocorrências de cada número nas duas metades
     int contadorEsquerda = 0, contadorDireita = 0;
-    for (int i = inicio; i <= fim; i++) {
+    for (int i = inicio; i <= fim; i++)
+    {
         if (vetor[i] == numeroEsquerda) contadorEsquerda++;
         if (vetor[i] == numeroDireita) contadorDireita++;
     }
 
     // Retornar o número que aparece mais vezes
-    if (contadorEsquerda > contadorDireita) {
+    if (contadorEsquerda > contadorDireita)
+    {
         return numeroEsquerda;
-    } else {
+    }
+    else
+    {
         return numeroDireita;
     }
 }
-int main()
-{
-    int n;
-    vector<int> v;
 
-    while(1)
-    {
-        cout << "Insira valor de n (se <= 0 finaliza): ";
-        cin >> n;
-        if(n <= 0) break;
+//-------------------------------------------------------------------------------------------------------------------------------
+int combinar(vector<int> &A, int inicio, int meio, int fim, int numeroEsquerda, int numeroDireita) {
+    int esq = inicio, dir = meio + 1;
+    int tam = fim - inicio + 1;
+    vector<int> D(tam);
+    int posD = 0;
 
-        vector<int> v = Gerador_De_Vetor(n);
-
-        clock_t inicioIterativo = clock();
-        int resultadoIterativo = NumeroMaisRepetidoIterativo(v, n);
-        clock_t fimIterativo = clock();
-        double tempoIterativoSeg = double(fimIterativo - inicioIterativo) / CLOCKS_PER_SEC;
-
-        cout << "Metodo | Resultado | Tempo (segundos)" << endl;
-        cout << "Iterativo | " << resultadoIterativo << " | " << tempoIterativoSeg << endl;
-
-        clock_t inicioDAC = clock();
-        int resultadoDAC = NumeroMaisRepetidoDivAndConquer(v, 0, n - 1);
-        clock_t fimDAC = clock();
-        double tempoDACSeg = double(fimDAC - inicioDAC) / CLOCKS_PER_SEC;
-
-        cout << "Divide And Conquer | " << resultadoDAC << " | " << tempoDACSeg << endl;
-
-        v.clear();
-        cout << "\n\n\n";
+    while (esq <= meio && dir <= fim) {
+        if (A[esq] <= A[dir]) {
+            D[posD++] = A[esq++];
+        } else {
+            D[posD++] = A[dir++];
+        }
+    }
+    while (esq <= meio) {
+        D[posD++] = A[esq++];
+    }
+    while (dir <= fim) {
+        D[posD++] = A[dir++];
     }
 
+    int contaEsq = 0, contaDir = 0, elemMax = D[0], elemAtual = D[0];
+    int contaMax = 0, contaAtual = 0;
+
+    for (int i = 0; i < tam; i++) {
+        if (D[i] == numeroEsquerda) {
+            contaEsq++;
+        } else if (D[i] == numeroDireita) {
+            contaDir++;
+        } else if (D[i] == elemAtual) {
+            contaAtual++;
+        } else {
+            if (contaMax < contaAtual) {
+                contaMax = contaAtual;
+                elemMax = elemAtual;
+            }
+            elemAtual = D[i];
+            contaAtual = 1;
+        }
+    }
+    if (contaMax < contaAtual) {
+        contaMax = contaAtual;
+        elemMax = elemAtual;
+    }
+
+    for (int i = 0; i < tam; i++) {
+        A[inicio + i] = D[i];
+    }
+
+    if (contaEsq > contaDir && contaEsq > contaMax) {
+        return numeroEsquerda;
+    } else if (contaDir > contaEsq && contaDir > contaMax) {
+        return numeroDireita;
+    }
+    return elemMax;
+}
+
+int majorMelhorado(vector<int> &A, int inicio, int fim) {
+    if (inicio == fim) {
+        return A[inicio];
+    }
+    int meio = inicio + (fim - inicio) / 2;
+    int numeroEsquerda = majorMelhorado(A, inicio, meio);
+    int numeroDireita = majorMelhorado(A, meio + 1, fim);
+    return combinar(A, inicio, meio, fim, numeroEsquerda, numeroDireita);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------
+int main()
+{
+    int n, opc = 0;
+    vector<int> v;
+    while(1)
+    {
+        //Escolha do tipo de vetor
+        printf("\n1) Vetor com elemento Maximal garantido (mais lento)\n2) Vetor com elementos aleatorios\n3)Sair\nEscolha:");
+        while(opc < 1 || opc > 3) cin >> opc;
+        if(opc == 3) break;
+
+        while(1){
+            //Inserção do tamanho do vetor
+            cout << "Insira valor de n (se <= 0 finaliza): ";
+            cin >> n;
+            if(n <= 0) break;
+
+            //Gera o vetor
+            if(opc ==2)v = Gerador_De_Vetor(n);
+            else v = Gerador_De_Vetor_Maximal(n);
+
+            //Calcula o tempo do iterativo
+            clock_t inicioIterativo = clock();
+            int resultadoIterativo = majorIterativo(v, n);
+            clock_t fimIterativo = clock();
+            double tempoIterativoSeg = double(fimIterativo - inicioIterativo) / CLOCKS_PER_SEC;
+
+            cout << "Metodo | Resultado | Tempo (segundos)" << endl;
+            cout << "Iterativo | " << resultadoIterativo << " | " << tempoIterativoSeg << endl;
+
+            //Calcula o tempo do Divide and Conquer
+            clock_t inicioDAC = clock();
+            int resultadoDAC = elementoMajoritarioDaC(v, 0, n - 1);
+            clock_t fimDAC = clock();
+            double tempoDACSeg = double(fimDAC - inicioDAC) / CLOCKS_PER_SEC;
+
+            cout << "Divide And Conquer | " << resultadoDAC << " | " << tempoDACSeg << endl;
+
+            //Calcula o tempo do aprimorado
+            clock_t inicioMelhor = clock();
+            int resultadoMelhor = majorMelhorado(v,0,n-1);
+            clock_t fimMelhor = clock();
+            double tempoMelhorSeg = double(fimMelhor - inicioMelhor)/CLOCKS_PER_SEC;
+
+            cout << "Divide And Conquer Melhorado | " << resultadoMelhor << " | " << tempoMelhorSeg << endl;
+            //Limpa o vetor
+            v.clear();
+            cout << "\n\n\n";
+        }
+        //Limpa o vetor se ele não estiver vazio
+        if(!v.empty()) v.clear();
+    }
     return 0;
 }
